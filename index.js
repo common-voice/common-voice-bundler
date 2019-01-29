dd43fe00a2acec3aa9933604a7a61d445c700a9b
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-01-29 13:42:22 -0500

    fix for AWS IAM/STS

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ const { accessKeyId, secretAccessKey, name: outBucketName } = config.get(
 );
 
 const outBucket = new S3({
-  credentials: {
-    accessKeyId,
-    secretAccessKey
-  },
   region: 'us-west-2'
 });
 const releaseDir = 'cv-corpus-' + new Date().toISOString();
@@ -41,7 +37,7 @@ const createAndUploadClipsTSVArchive = () => {
   if (!config.get('skipBundling')) {
     managedUpload = outBucket.upload({
       Body: archivePassThrough,
-      Bucket: 'common-voice-data-download',
+      Bucket: outBucketName,
       Key: `${releaseDir}/clips.tsv.zip`
     });
   }
@@ -60,10 +56,6 @@ const getClipFile = path => {
     'clipBucket'
   );
   return new S3({
-    credentials: {
-      accessKeyId,
-      secretAccessKey
-    },
     region
   }).getObject({
     Bucket: name,
