aaadf3726167720ff955b4ae5959e6b4ea564ad8
Gabriel <gabriel@mozillafoundation.org>
2022-09-15 16:25:54 -0400

    Remove unused function

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const {
   saveStatsToDisk,
   loadStatsFromDisk,
 } = require('./processStats');
-const { getDuration } = require('./getDuration');
+// const { getDuration } = require('./getDuration');
 const { db, clipBucket, bundlerBucket } = require('./init').initialize();
 
 /**
