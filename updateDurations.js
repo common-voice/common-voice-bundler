cfaf506396b38a176bbe54d76853c66eb387d4db
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-03-09 10:35:05 +0100

    feat: add retry logic

--- updateDurations.js
+++ updateDurations.js
@@ -7,11 +7,14 @@ const { loadStatsFromDisk } = require('./processStats');
 
 const db = connectToDb();
 const getTimesPathFromLocale = (locale) =>
-  path.join(__dirname, RELEASE_NAME, locale, 'times.txt');
+  path.join(RELEASE_PATH, locale, 'times.txt');
 const getFileContents = (path) => fs.readFileSync(path, 'utf-8');
 
 const RELEASE_NAME = config.get('releaseName');
+const RELEASE_PATH = path.join(__dirname, RELEASE_NAME)
 const BATCH_SIZE = 5000;
+
+const PROCESSED_BATCHES_FILE = 'processedBatches.tsv'
 
 const extractIdFromFilePathString = (filepath) => {
   return +filepath
@@ -39,12 +42,24 @@ const buildValues = (id, duration) => {
   return `(${id}, client_id, path, sentence, original_sentence_id, ${duration})`;
 };
 
-const batchUpdateClipsTable = async (idsAndDurations) => {
+const batchUpdateClipsTable = async (idsAndDurations, locale) => {
+  const processedBatchesFilepath = path.join(RELEASE_PATH, locale, PROCESSED_BATCHES_FILE)
   const values = idsAndDurations.map(({ id, duration }) => buildValues(id, duration));
   const total = values.length;
 
   let current = 0;
   let start = 0;
+
+  if (process.env.npm_config_retry) {
+    // processedBatches.tsv might not exist yet
+    try {
+      start = parseInt(await fs.promises.readFile(processedBatchesFilepath))
+    } catch (err) {
+      start = 0
+    }
+
+    console.log(`Retry batches from ${start}`);
+  }
 
   while (true) {
     const end = start + BATCH_SIZE;
@@ -78,7 +93,7 @@ const batchUpdateClipsTable = async (idsAndDurations) => {
         (err, results, fields) => {
           if (err) {
             return db.rollback(() => { throw err; });
-          };
+          }
 
           db.commit((err) => {
             if (err) {
@@ -87,13 +102,16 @@ const batchUpdateClipsTable = async (idsAndDurations) => {
 
             current += batch.length;
             start = end;
+
             console.log(`Processed ${results.affectedRows} rows.`);
             console.log(`Progress: ${current}/${total}\n\n`);
           });
         }
-      );
-    });
-
+        );
+      });
+      
+    await fs.promises.writeFile(processedBatchesFilepath, '' + current + '\n')
+    
     await new Promise(resolve => setTimeout(resolve, 5000));
   }
 };
@@ -102,9 +120,9 @@ const run = async () => {
   const locales = loadStatsFromDisk(RELEASE_NAME).locales;
 
   for (const locale of locales) {
-    const idsAndLocales = getIdsAndDurationFromLocale(locale);
+    const idsAndDurations = getIdsAndDurationFromLocale(locale);
     console.log(`Starting update for locale: '${locale}'`);
-    await batchUpdateClipsTable(idsAndLocales);
+    await batchUpdateClipsTable(idsAndDurations, locale);
   }
 };
 
